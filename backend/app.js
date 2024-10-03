6b2be40b393796e58a6627bb4a153745b2abeb9c
ErickTrujilloOsorio <ericktrujillo011@gmail.com>
2024-10-02 21:00:47 -0600

    ordenar app.js

--- backend/app.js
+++ backend/app.js
@@ -1,36 +1,54 @@
+// Importaciones
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { sequelize } = require('./config/db');
 
+// Inicializamos la aplicación Express
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+
+// Configuración del puerto
 const PORT = process.env.PORT || 3000;
 
-// Probamos la conexión y sincronizamos modelos
-sequelize.sync().then(() => {
-  console.log('Modelos sincronizados con la base de datos.');
-  app.listen(PORT, () => {
-    console.log(`Servidor escuchando en el puerto ${PORT}`);
-  });
-}).catch(err => {
-  console.error('Error al sincronizar los modelos:', err);
-}
-);
-
+// Aplicar esto solo si hay cambios en los modelos
+/*
 // Importamos modelos
-const tipoEmpleado = require('./models/tipo_empleado');
-const estado = require('./models/estado');
-const credito = require('./models/credito');
-const cliente = require('./models/cliente');
-const empleado = require('./models/empleado');
-const solicitud = require('./models/solicitud');
+require('./models/tipo_empleado');
+require('./models/estado');
+require('./models/credito');
+require('./models/cliente');
+require('./models/empleado');
+require('./models/solicitud');
+*/
 
 // Importamos rutas
 const clientesRoutes = require('./routes/clientes');
 
 // Endpoints
-app.use('/clientes',  clientesRoutes);
+app.use('/clientes', clientesRoutes);
 
+// Conexión a la base de datos y sincronización de modelos
+sequelize.authenticate()
+    .then(() => {
+        console.log('Conexión a la base de datos establecida con éxito.');
 
+        // HABILITAR SOLAMENTE SI HAY CAMBIOS EN LOS MODELOS
+        // return sequelize.sync();  
+    })
+    .then(() => {
+        // console.log('Modelos sincronizados con la base de datos.');
+        // Iniciar el servidor después de la sincronización
+        app.listen(PORT, () => {
+            console.log(`Servidor escuchando en el puerto ${PORT}`);
+        });
+    })
+    .catch(err => {
+        console.error('Error al conectar a la base de datos o al sincronizar los modelos:', err);
+    });
+
+// Rutas de ejemplo (opcional)
+app.get('/', (req, res) => {
+    res.send('hola');
+});
