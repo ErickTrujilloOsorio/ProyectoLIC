ae1c1e43133f07062cac7148c5b3db5f5ffa2ad3
PF211251 <geofuentes.gf@gmail.com>
2024-10-14 19:55:09 -0600

    Cambiar estado de solicitud funcionando

--- backend/controllers/solicitudController.js
+++ backend/controllers/solicitudController.js
@@ -1,12 +1,12 @@
 const { sequelize } = require('../config/db');
 const Solicitud = require('../models/solicitud');
-
+const Estado = require('../models/estado'); 
 exports.obtenerSolicitudes = async (req, res) => {
     try {
         const [solicitudes] = await sequelize.query(
             `SELECT DISTINCT s.idSolicitud, c.nombre_cliente, c.idCliente, c.apellido_cliente, 
                     e.nombre_empleado, e.apellido_empleado, 
-                    est.nombre_estado, est.descripcion_estado AS estado_descripcion, 
+                    est.estado, est.descripcion_estado AS estado_descripcion, 
                     cr.nombre_credito, cr.interes
              FROM solicitudes s
              INNER JOIN clientes c ON s.cliente_id = c.idCliente
@@ -63,5 +63,32 @@ exports.asignarEmpleado = async (req, res) => {
     } catch (error) {
         console.error('Error al asignar empleado:', error);
         res.status(500).json({ message: "Error al asignar empleado", error: error.message });
+    }
+};
+
+// Asignar un estado a una solicitud, esperemos funcione pa!
+exports.asignarEstado = async (req, res) => {
+    const { idSolicitud, estado_id } = req.body; 
+    try {
+        const solicitud = await Solicitud.findByPk(idSolicitud);
+        const estado = await Estado.findByPk(estado_id);  // Busca el estado usando el modelo Estado
+
+        // Verificar si la solicitud y el estado existen
+        if (!solicitud) {
+            return res.status(404).json({ message: 'Solicitud no encontrada' });
+        }
+
+        if (!estado) {
+            return res.status(404).json({ message: 'Estado no encontrado' });
+        }
+
+        // Asignar el nuevo estado
+        solicitud.estado_id = estado_id;
+        await solicitud.save();
+
+        res.status(200).json({ message: 'Estado asignado correctamente', solicitud });
+    } catch (error) {
+        console.error('Error al asignar estado:', error);
+        res.status(500).json({ message: "Error al asignar estado", error: error.message });
     }
 };
--- backend/routes/solicitudes.js
+++ backend/routes/solicitudes.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const solicitudController = require('../controllers/solicitudController');
-
 router.get('/get', solicitudController.obtenerSolicitudes);
 router.post('/asignar', solicitudController.asignarEmpleado);
-
+router.post('/asignarEstado', solicitudController.asignarEstado);
 module.exports = router;
